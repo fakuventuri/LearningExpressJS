ce574dda60b474e536f871f405933ade7742198c
Facundo Tomas Acosta <acostafacundotomas@gmail.com>
2023-06-05 14:08:24 -0300

    Add divideValues

--- sumArrayValues.js
+++ sumArrayValues.js
@@ -6,8 +6,13 @@ function multiplyValues(val1, val2) {
   return val1 * val2;
 }
 
+function divideValues(val1, val2) {
+  return val1 / val2;
+}
+
 const value1 = 8;
 const value2 = 4;
 
 console.log("Sum: " + sumValues(value1, value2));
 console.log("Multiply: " + multiplyValues(value1, value2));
+console.log(`Divide: ${divideValues(value1, value2)}`);
