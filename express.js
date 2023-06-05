31512372b04ee5c6d9b9cf961a22406b60793c2b
Facundo Venturi <81877086+fakuventuri@users.noreply.github.com>
2023-06-05 13:17:00 -0300

    sumArrayValues

--- express.js
+++
@@ -1,7 +0,0 @@
-console.log("express.js");
-
-console.log("prueba");
-
-let suma = 2 + 2;
-
-console.log(suma);
---
+++ sumArrayValues.js
@@ -0,0 +1,11 @@
+function sumArrayValues(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
+}
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(sumArrayValues(array));
