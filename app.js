dc479af5c0bb196c31715e1ce90564db85a6d613
Facundo Venturi <81877086+fakuventuri@users.noreply.github.com>
2023-06-05 16:13:32 -0300

    Test web with fetching

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const datos = { message: "Facu te amo", name: "Jorge" };
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/home", (req, res) => {
+  res.redirect("/datos");
+});
+
+app.get("/datos", (req, res) => {
+  res.json(datos);
 });
 
 app.listen(port, () => {
