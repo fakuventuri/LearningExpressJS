a0b898064d41d828aec3f0e7bb73953653d12015
Facundo Venturi <81877086+fakuventuri@users.noreply.github.com>
2023-06-05 14:29:01 -0300

    Update sumArrayValues.js

--- sumArrayValues.js
+++ sumArrayValues.js
@@ -6,8 +6,8 @@ function multiplyValues(val1, val2) {
   return val1 * val2;
 }
 
-function divideValues(val1, val2) {
-  return val1 / val2;
+function substractValues(val1, val2) {
+  return val1 - val2;
 }
 
 const value1 = 8;
@@ -15,4 +15,4 @@ const value2 = 4;
 
 console.log("Sum: " + sumValues(value1, value2));
 console.log("Multiply: " + multiplyValues(value1, value2));
-console.log(`Divide: ${divideValues(value1, value2)}`);
+console.log(`Substract: ${substractValues(value1, value2)}`);
